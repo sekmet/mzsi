64a3c57e3fd8c0dd63773b0613d80ca0114c5f72
Cauê Alves <ceasbz2@gmail.com>
2016-10-17 00:57:20 -0200

    Removed redundant key in possibilities list

--- index.js
+++ index.js
@@ -17,73 +17,61 @@ module.exports = (month, day, languageCode) => {
   const possibilities = [
     {
       id: 1,
-      name: 'aquarius',
       begin: [20, 1],
       end: [18, 2]
     },
     {
       id: 2,
-      name: 'pisces',
       begin: [19, 2],
       end: [20, 3]
     },
     {
       id: 3,
-      name: 'aries',
       begin: [21, 3],
       end: [19, 4]
     },
     {
       id: 4,
-      name: 'taurus',
       begin: [20, 4],
       end: [20, 5]
     },
     {
       id: 5,
-      name: 'gemini',
       begin: [21, 5],
       end: [20, 6]
     },
     {
       id: 6,
-      name: 'cancer',
       begin: [21, 6],
       end: [21, 7]
     },
     {
       id: 7,
-      name: 'leo',
       begin: [22, 7],
       end: [22, 8]
     },
     {
       id: 8,
-      name: 'virgo',
       begin: [23, 8],
       end: [22, 9]
     },
     {
       id: 9,
-      name: 'libra',
       begin: [23, 9],
       end: [22, 10]
     },
     {
       id: 10,
-      name: 'scorpio',
       begin: [23, 10],
       end: [21, 11]
     },
     {
       id: 11,
-      name: 'sagittarius',
       begin: [22, 11],
       end: [21, 12]
     },
     {
       id: 12,
-      name: 'capricorn',
       begin: [22, 12],
       end: [19, 1]
     }
